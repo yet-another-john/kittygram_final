3d598399ffa0bc6258c50b4294ac969fa5c94275
Evgenii Stoliarov <stoliarovea@gmail.com>
2024-07-24 18:00:30 +0300

    24.07.2024-2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -98,16 +98,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-#STATIC_URL = '/static/'
-
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/kittygram/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
